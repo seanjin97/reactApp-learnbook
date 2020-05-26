7ca8310faaac1f63064399d2bf252595760a5b6f
Sean Jin <seanjin.lee.2018@sis.smu.edu.sg>
2020-05-26 23:30:26 +0800

    Updates

---
+++ precache-manifest.acb6d59a249a27f36bb5807a47772eab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f8442dc57e28fe6345ef662aa57113c",
+    "url": "/reactApp-learnbook/index.html"
+  },
+  {
+    "revision": "63962cc45c930472843b",
+    "url": "/reactApp-learnbook/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8ccd25c82668498cfde5",
+    "url": "/reactApp-learnbook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "63962cc45c930472843b",
+    "url": "/reactApp-learnbook/static/js/2.78e75b79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactApp-learnbook/static/js/2.78e75b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ccd25c82668498cfde5",
+    "url": "/reactApp-learnbook/static/js/main.bf972d00.chunk.js"
+  },
+  {
+    "revision": "4d48404bd916922b6cb4",
+    "url": "/reactApp-learnbook/static/js/runtime-main.a9ff4e07.js"
+  }
+]);
